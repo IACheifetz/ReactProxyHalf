896c307950b22060581708e180eb81803e805ed0
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-05-17 17:45:20 -0700

    initial commit

--- functions/pokemon.js
+++ functions/pokemon.js
@@ -16,14 +16,20 @@ exports.handler = async (event, context) => {
     // consult the pokedex docs 
     // https://pokedex-alchemy.herokuapp.com/
 
+    
+    const response = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${event.queryStringParameters.searchFilter}`);
+    
+    const data = await response.json();
+    const json = JSON.stringify({ data });
+
     return { 
       statusCode: 200, 
       headers,
     // this is where you shoot data back to the user. right now it's sending an empty object--replace this with the pokemon data. remember, you do need to stringify it, otherwise netlify gets mad. ¯\_(ツ)_/¯
-      body: JSON.stringify({}),
+      body: json,
     };
   } catch (error) {
-    console.log(error);
+    console.log(error); //eslint-disable-line
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed fetching data' }),
